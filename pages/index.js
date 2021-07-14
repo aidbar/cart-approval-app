6868f65f35bb69d5e6ed8da4682e41016ea4b4f8
aidbar <aide.barniskyte@tuni.fi>
2021-07-14 13:27:01 +0300

    Context added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getAllCartsData } from '../lib/carts'
+import AppContext from '../contexts/appContext'
 
 
 export default function Home({ allCartsData }) {
